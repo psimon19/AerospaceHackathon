30a74b5adb6fb79f77d3d2d64d98616af9a9e857
Peter Simon <psimon@gatech.edu>
2016-11-05 19:58:05 -0400

    fixed some math

--- motorcode.py
+++ motorcode.py
@@ -19,26 +19,27 @@ GPIO.setup(dir, GPIO.OUT)
 count = 0
 
 #Variables
-seconds
-SIDRATE = .0000727
-TPI = 16
-REV = 1/16 * TPI
-SPR = 3200	
+SIDRATE = .0000727	#Sidereal rate
+TPI = 16			#Thread per inch
+REV = 1/16 * TPI	#Revolution
+SPR = 3200			#Steps per revolution
+ARCCORRECTION = 0 	#Correction for arc
 
 dThetaMu = 		#Change in theta for 1 mu step
 #ThetaR	 = 		#Change in theta for 1 revolution
 
 
 dlRod = 1/16		#Change in length of the rod
-dlRodMu = 
+dlRodMu				#Change in length of rod per step
 lArms = 			#Length of arms
+seconds				#Seconds between change
 
 #Time in seconds between 
 def sleepTime():
 	#dThetaR = math.acos((dlRod ** 2) / ((2) * (lArms ** 2)))
 	dlRodMu = (dlRod) * (1 / SPR)
 	dThetaMu = math.acos((dlRodMu ** 2) / ((2) * (lArms ** 2)))
-	seconds = dThetaMu / SIDRATE
+	seconds = (dThetaMu + ARCCORRECTION) / SIDRATE
 	return seconds
 
 def resetBEDPins():
