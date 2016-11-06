de2fcd397222e82c11b6c106283b81d1b4efbebe
Jon Dolan <jondolan33@gmail.com>
2016-11-06 09:42:29 -0500

    Web code, thanks Peter

--- webFrontMotorCode.py
+++ webFrontMotorCode.py
@@ -24,36 +24,37 @@ urls = (
     '/', 'index'
 )
 
+class index:
+    def GET(self):
+        return "Hello world!"
+
 #Variables
 SIDRATE = .0000727	#Sidereal rate
 TPI = 28.0			#Thread per inch
-REV = 1.0/28 * TPI	#Revolution
-SPR = 3200			#Steps per revolution
+REV = 1			#Revolution
+SPR = 3200		#Steps per revolution
 ARCCORRECTION = 0.0	#Correction for arc
 
-dThetaMu = 0.0 		#Change in theta for 1 micro step
-ThetaR	= 0.0 		#Change in theta for 1 revolution
 
+#dThetaMu 		#Change in theta for 1 micro step
+dThetaR	= 0.0 		#Change in theta for 1 revolution
 
 dlRod = 1.0/TPI		#Change in length of the rod (inch)
-dlRodMu				#Change in length of rod per step (inch)
+#dlRodMu				#Change in length of rod per step (inch)
 
-lArms = 8.5			#Length of arms (inch)
+lArms = 8.5
+			#Length of arms (inch)
 seconds	= 0.0		#Seconds between change
 
-debugVar = 0			#debug variable
 
-class index:
-    def GET(self):
-        return "Hello World"
 
 #Time in seconds between
 def sleepTime():
 	dThetaR = math.acos(1 - ((dlRod ** 2) / ((2) * (lArms ** 2)))) / SPR
-	dlRodMu = (dlRod) * (1 / SPR)
-	dThetaMu = math.acos(1 - (dlRodMu ** 2) / ((2) * (lArms ** 2)))
+	#dlRodMu = (dlRod) * (1 / SPR)
+	#dThetaMu = math.acos(1 - (dlRodMu ** 2) / ((2) * (lArms ** 2)))
 	#print dThetaR
-	seconds = (dThetaMu + ARCCORRECTION) / SIDRATE
+	seconds = (dThetaR + ARCCORRECTION) / SIDRATE
 	return seconds / 2
 
 def resetBEDPins():
@@ -73,9 +74,9 @@ def smallStepMode():
 	GPIO.output(MS3, GPIO.HIGH)
 	for i in range(0, 200):
 		GPIO.output(stp, GPIO.HIGH)
-		time.sleep(0, sleepTime)
+		time.sleep(sleepTime)
 		GPIO.output(stp, GPIO.LOW)
-		time.sleep(0, sleepTime)
+		time.sleep(sleepTime)
 
 while (True):
 	GPIO.output(EN, GPIO.LOW)
