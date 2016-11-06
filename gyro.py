06a7a9aff381c6569b62a251b8f0a95c00daacfd
Peter Simon <psimon@gatech.edu>
2016-11-06 10:58:27 -0500

    revert to previous version

--- gyro.py
+++ gyro.py
@@ -68,49 +68,56 @@ while (calibrateTrue is not "y"):
 calibrate()
 while (True):
 
+	cx = 0
+	cy = 0
+	cz = 0
 	
-	# MMA8452Q address, 0x1C(28)
-	# Select Control register, 0x2A(42)
-	#		0x00(00)	StandBy mode
-	bus.write_byte_data(0x1D, 0x2A, 0x00)
+	for i in range(0, 10):
+		# MMA8452Q address, 0x1C(28)
+		# Select Control register, 0x2A(42)
+		#		0x00(00)	StandBy mode
+		bus.write_byte_data(0x1D, 0x2A, 0x00)
 
-	# MMA8452Q address, 0x1C(28)
-	# Select Control register, 0x2A(42)
-	#		0x01(01)	Active mode
-	bus.write_byte_data(0x1D, 0x2A, 0x01)
+		# MMA8452Q address, 0x1C(28)
+		# Select Control register, 0x2A(42)
+		#		0x01(01)	Active mode
+		bus.write_byte_data(0x1D, 0x2A, 0x01)
 
-	# MMA8452Q address, 0x1C(28)
-	# Select Configuration register, 0x0E(14)
-	#		0x00(00)	Set range to +/- 2g
-	bus.write_byte_data(0x1D, 0x0E, 0x00)
+		# MMA8452Q address, 0x1C(28)
+		# Select Configuration register, 0x0E(14)
+		#		0x00(00)	Set range to +/- 2g
+		bus.write_byte_data(0x1D, 0x0E, 0x00)
 
-	time.sleep(0.1)
+		time.sleep(0.1)
 
-	# MMA8452Q address, 0x1C(28)
-	# Read data back from 0x00(0), 7 bytes
-	# Status register, X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
-	data = bus.read_i2c_block_data(0x1D, 0x00, 7)
+		# MMA8452Q address, 0x1C(28)
+		# Read data back from 0x00(0), 7 bytes
+		# Status register, X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
+		data = bus.read_i2c_block_data(0x1D, 0x00, 7)
 
-	#except IOError:
-	#	subprocess.call(['i2cdetect', '-y', '1'])
-	#	flag = 1
+		#except IOError:
+		#	subprocess.call(['i2cdetect', '-y', '1'])
+		#	flag = 1
 
-	# Convert the data
-	xAccl = (data[1] * 256 | data[2]) / 16 - xAcclCal
-	if xAccl > 2047 :
-		xAccl -= 4096
+		# Convert the data
+		xAccl = (data[1] * 256 | data[2]) / 16 - xAcclCal
+		if xAccl > 2047 :
+			xAccl -= 4096
 
-	yAccl = (data[3] * 256 | data[4]) / 16 - yAcclCal
-	if yAccl > 2047 :
-		yAccl -= 4096
+		yAccl = (data[3] * 256 | data[4]) / 16 - yAcclCal
+		if yAccl > 2047 :
+			yAccl -= 4096
 
-	zAccl = (data[5] * 256 | data[6]) / 16 - zAcclCal
-	if zAccl > 2047 :
-		zAccl -= 4096
+		zAccl = (data[5] * 256 | data[6]) / 16 - zAcclCal
+		if zAccl > 2047 :
+			zAccl -= 4096
 
-	cx = (float(xAccl) / float(1<<11) * float(8)) * 22.5
-	cy = (float(yAccl) / float(1<<11) * float(8)) * 22.5
-	cz = (float(zAccl) / float(1<<11) * float(8)) * 22.5
+		cx += (float(xAccl) / float(1<<11) * float(8)) * 22.5
+		cy += (float(yAccl) / float(1<<11) * float(8)) * 22.5
+		cz += (float(zAccl) / float(1<<11) * float(8)) * 22.5
+	cx = cx / 10
+	cy = cy / 10
+	cz = cz / 10
 	
 	# Output data to screen
 	print "Acceleration in X-Axis : %4f" %cx
