f600912c1c233f0edd7c1a142539edd6a007fa13
Peter Simon <psimon@gatech.edu>
2016-11-06 01:01:18 -0500

    added a while loop to gyro.py

--- gyro.py
+++ gyro.py
@@ -6,53 +6,51 @@
 
 import smbus
 import time
-import subprocess
 
-#try:
 # Get I2C bus
 
-bus = smbus.SMBus(1)
-print "--"
-# MMA8452Q address, 0x1C(28)
-# Select Control register, 0x2A(42)
-#		0x00(00)	StandBy mode
-bus.write_byte_data(0x1D, 0x2A, 0x00)
-print "00"
-# MMA8452Q address, 0x1C(28)
-# Select Control register, 0x2A(42)
-#		0x01(01)	Active mode
-bus.write_byte_data(0x1D, 0x2A, 0x01)
-print "01"
-# MMA8452Q address, 0x1C(28)
-# Select Configuration register, 0x0E(14)
-#		0x00(00)	Set range to +/- 2g
-bus.write_byte_data(0x1D, 0x0E, 0x00)
-print "02"
-time.sleep(0.5)
+while (True):
+	bus = smbus.SMBus(1)
+	# MMA8452Q address, 0x1C(28)
+	# Select Control register, 0x2A(42)
+	#		0x00(00)	StandBy mode
+	bus.write_byte_data(0x1D, 0x2A, 0x00)
 
-# MMA8452Q address, 0x1C(28)
-# Read data back from 0x00(0), 7 bytes
-# Status register, X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
-data = bus.read_i2c_block_data(0x1D, 0x00, 7)
+	# MMA8452Q address, 0x1C(28)
+	# Select Control register, 0x2A(42)
+	#		0x01(01)	Active mode
+	bus.write_byte_data(0x1D, 0x2A, 0x01)
 
-#except IOError:
-#	subprocess.call(['i2cdetect', '-y', '1'])
-#	flag = 1
+	# MMA8452Q address, 0x1C(28)
+	# Select Configuration register, 0x0E(14)
+	#		0x00(00)	Set range to +/- 2g
+	bus.write_byte_data(0x1D, 0x0E, 0x00)
 
-# Convert the data
-xAccl = (data[1] * 256 + data[2]) / 16
-if xAccl > 2047 :
+	time.sleep(0.1)
+
+	# MMA8452Q address, 0x1C(28)
+	# Read data back from 0x00(0), 7 bytes
+	# Status register, X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
+	data = bus.read_i2c_block_data(0x1D, 0x00, 7)
+
+	#except IOError:
+	#	subprocess.call(['i2cdetect', '-y', '1'])
+	#	flag = 1
+
+	# Convert the data
+	xAccl = (data[1] * 256 + data[2]) / 16
+	if xAccl > 2047 :
 	xAccl -= 4096
 
-yAccl = (data[3] * 256 + data[4]) / 16
-if yAccl > 2047 :
+	yAccl = (data[3] * 256 + data[4]) / 16
+	if yAccl > 2047 :
 	yAccl -= 4096
 
-zAccl = (data[5] * 256 + data[6]) / 16
-if zAccl > 2047 :
+	zAccl = (data[5] * 256 + data[6]) / 16
+	if zAccl > 2047 :
 	zAccl -= 4096
 
-# Output data to screen
-print "Acceleration in X-Axis : %d" %xAccl
-print "Acceleration in Y-Axis : %d" %yAccl
-print "Acceleration in Z-Axis : %d" %zAccl
+	# Output data to screen
+	print "Acceleration in X-Axis : %d" %xAccl
+	print "Acceleration in Y-Axis : %d" %yAccl
+	print "Acceleration in Z-Axis : %d" %zAccl
